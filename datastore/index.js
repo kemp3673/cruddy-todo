e2ade79ef242e27488cd09b990e5a5a6f26ac323
vpatel89 <virenpatel89@gmail.com>
2022-06-22 13:29:15 -0400

    Completed deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -9,60 +9,132 @@ var items = {};
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId(function(err, uniqueData) {
-    console.log('uniqueData: ', uniqueData);
     let id = uniqueData;
-    fs.writeFile(exports.dataDir + '/' + id, text, (err) => {
-      console.log('test');
-      // Makes file per ID but need to direct to correct folder
+    fs.writeFile(exports.dataDir + '/' + id + '.txt', text, (err) => {
+      callback(null, {id, text});
     });
   });
-  // var id = counter.getNextUniqueId();
-  //console.log('id: ', id);
-  //items[id] = text;
-  //callback(null, { id, text });
 };
-
-// fs.writeFile('newfile.txt', "Hello world", (err) => {
-//   if (err) return callback(err);
-//   console.log("file created successfully!")
-// });
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  var resultData = [];
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (files.length === 0) {
+      callback(null, []);
+    } else {
+      files.forEach(function(filename) {
+        fs.readFile(exports.dataDir + '/' + filename, 'utf-8', function(err, data) {
+          resultData.push({text: data});
+        });
+      });
+    }
   });
-  callback(null, data);
+  done(callback(null, resultData));
 };
 
+
+//promise.all
+// Promise.prototype.finally()
+// Promise.prototype.catch()
+// var resultData = [];
+// var filesList = [];
+// fs.readdir(exports.dataDir, (err, files) => {
+//   if (files.length === 0) {
+//     callback(null, []);
+//   } else {
+//     files.forEach(function(filename) {
+//       fs.readFile(exports.dataDir + '/' + filename, 'utf-8', function(err, data) {
+//         filesList.push({file: filename, text: data});
+//       });
+//       console.log('List: ', filesList);
+//     });
+//   }
+// });
+
+
+
+// var resultData = [];
+// console.log(fs.readdir(exports.dataDir));
+// fs.promises(fs.readdir(exports.dataDir, files))
+//   .then(
+//     files.forEach(function(filename) {
+//       fs.readFile(exports.dataDir + '/' + filename, 'utf-8', function(err, data) {
+//         resultData.push({text: data});
+//       });
+//     })
+//   );
+//.thenfinally(callback(null, resultData));
+// };
+
+// fs.promises.readdir(targetDir)
+
+//     // If promise resolved and
+//     // datas are fetched
+//     .then(filenames => {
+//         for (let filename of filenames) {
+//             console.log(filename)
+//         }
+//     })
+
+//     // If promise is rejected
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+
+
+// var resultData = [];
+// fs.readdir(exports.dataDir, (err, files) => {
+//   //console.log('1');
+//   // if (files.length === 0) {
+//   //   callback(null, []);
+//   // } else {
+//   files.forEach(function(filename) {
+//     fs.readFile(exports.dataDir + '/' + filename, 'utf-8', function(err, data) {
+//       resultData.push({text: data});
+//     });
+//   });
+//   //console.log('4');
+//   //console.log('array: ', resultData);
+//   callback(null, resultData);
+
+
+
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(exports.dataDir + '/' + id + '.txt', 'utf-8', function(err, data) {
+    if (err) {
+      callback(new Error('Not There Yo'));
+    } else {
+      callback(null, { id, text: data });
+    }
+  });
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (!files.includes(id + '.txt')) {
+      callback(new Error('File No Exist'));
+    } else {
+      fs.writeFile(exports.dataDir + '/' + id + '.txt', text, (err) => {
+        callback(null, { id, text });
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(exports.dataDir + '/' + id + '.txt', (err) => {
+    if (err) {
+      callback(new Error('Error while deleting file'));
+    } else {
+      callback('Deleted');
+    }
+  });
 };
+
+
+
+
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
